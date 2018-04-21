6b325b06101513889c579bbf3d0047d034b729c0
sinan <sinan@sinan-pc>
2018-04-21 10:59:26 +0300

    spring initializr üzerinden proje alt yapısı oluşturuldu.

---
+++ src/main/java/com/sy/romannums/RomanNumsApplication.java
@@ -0,0 +1,20 @@
+package com.sy.romannums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * Roma rakamı uygulamasının giriş noktasıdır. 
+ * 
+ * @author sinan.yumak
+ *
+ */
+@SpringBootApplication
+public class RomanNumsApplication {
+
+	public static void main( String[] args ) {
+		SpringApplication.run( RomanNumsApplication.class, args );
+	}
+	
+}
